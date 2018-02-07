8e99b6c891667d22145fad648199c676ff386ec3
imane <elhirchimane21@gmail.com>
2018-02-07 17:32:14 +0000

    first commit

---
+++ Desktop/github/GestionVols/src/com/bo/Reservation.java
@@ -0,0 +1,22 @@
+package com.bo;
+
+public class Reservation {
+	
+	int idReser;
+
+	public int getIdReser() {
+		return idReser;
+	}
+
+	public void setIdReser(int idReser) {
+		this.idReser = idReser;
+	}
+
+	@Override
+	public String toString() {
+		return "Reservation [idReser=" + idReser + "]";
+	}
+	
+	
+
+}
